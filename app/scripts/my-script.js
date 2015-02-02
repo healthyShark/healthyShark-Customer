0dc99ee29f01381caebc713582b331e3e95b80ed
robins gupta <robinskumar73@gmail.com>
2015-02-02 17:54:29 +0530

    First commit

---
+++ app/scripts/my-script.js
@@ -0,0 +1,32 @@
+(function () {
+    'use strict';
+    // this function is strict...
+
+    var nav = $('#nav-header'),
+        pos = nav.offset();
+    
+//    $(document).ready(function () {
+//        $(document).scroll(function () {
+//            if($(this).scrollTop() > pos.top+nav.height() && nav.hasClass('title')){
+//                console.log("adding fixed");
+//                $('#nav-header').removeClass('title').addClass('fixed');
+//            }
+//            else{
+//                
+//                $('#nav-header').removeClass('fixed').addClass('title');
+//            }
+//        });
+//    });
+
+
+
+//    $(function () {
+//        $('#slider').slider({
+//			value: 100,
+//            animate : 'fast',
+//            orientation: 'horizontal'
+//	    });
+//    });
+    
+}());
+
